8e65940d4a4edb98918b85269745c74924b65049
soumyayalahankaajjegowdagit config --global user.name soumyayalahankaajjegowda <soumya.ajjegowda@gmail.com>
2025-02-24 18:43:50 -0700

    latestupdate

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState, useReducer } from 'react';
+import AppName from './components/AppName'
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/components/todolist.jsx
@@ -0,0 +1,13 @@
+function AddTodo() {
+  return (
+  <div class="container   text-center">
+  <div class="row kg-row">
+          <div class="col-6"><input type="text" placeholder="Enter Todo Here" /></div>
+          <div class="col-4">Create Mockup</div>
+          <div class="col-2"><button type="button" class="btn btn-success kg-button">ADD</button></div>
+          </div>
+          </div>
+);
+}
+
+export default AddTodo;
